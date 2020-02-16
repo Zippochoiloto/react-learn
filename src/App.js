1697f9e8e8bc18f956afa2d22a6e5f6488b3a02e
Zippochoiloto94 <theanhptit94@gmail.com>
2020-02-16 11:56:42 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      title : 'App title'
+    }
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onSubmit(event) {
+    event.preventDefault();
+    console.log(this.input.value)
+    // alert('submitted')
+  }
+
+  render(){
+    // const list = [
+    //   'Item 1',
+    //   'Item 2',
+    //   'Another item'
+    // ];
+
+    return (
+      <div className = 'App'>
+        <h1>
+          {this.state.title}
+        </h1>
+        <form onSubmit = {this.onSubmit}>
+          <input onChange = {this.onChange} ref = {input =>this.input = input}/>
+        </form>
+      </div>
+    )
+  }
 }
 
 export default App;
